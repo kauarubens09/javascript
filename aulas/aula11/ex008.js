d745909a323e7a84fcf36081f25bd380b136a853
kauarubens09 <kauarubens09@gmail.com>
2025-10-28 19:53:06 -0300

    k

--- aulas/aula11/ex008.js
+++ aulas/aula11/ex008.js
@@ -1,1 +1,7 @@
-console.log('O console funcionou normalmente')
+var vel = 59
+console.log(`Você está dirigindo a ${vel}km/h`)
+if (vel>60){
+    console.log('Muito rápido multado!')
+}
+console.log('dirija com cuidado')
+
---
+++ aulas/aula11/ex009.js
@@ -0,0 +1,1 @@
+var pais = 'França'
