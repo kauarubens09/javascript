ad79f5bcdca799d32b8099bed25023cd5bec3293
kauarubens09 <kauarubens09@gmail.com>
2025-10-28 20:16:56 -0300

    k

--- aulas/aula11/ex009.js
+++ aulas/aula11/ex009.js
@@ -1,1 +1,8 @@
-var pais = 'França'
+var pais = 'Brasil'
+console.log(`Vivendo na ${pais}`)
+if (pais=='Brasil'){
+    console.log('Você é brasileiro')
+}
+else{
+    console.log('Você é estrangeiro')
+}
