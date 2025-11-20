7ca3469c7e6f55b8054610800a6c07aab32307df
kauarubens09 <kauarubens09@gmail.com>
2025-11-20 11:37:33 -0300

    terminado

--- aulas/aula16ex/ex018/script.js
+++ aulas/aula16ex/ex018/script.js
@@ -1,25 +1,73 @@
-let num = document.querySelector('input#fnum')
-let lista = document.querySelector('select#flista')
-let res = document.querySelector('div#rse')
-let valores = []
-function isNumero(n){
-    if (Number(n)>=1 && Number(n) <=100){
-        return true
-    }else{
-        return false
+let mum = document.querySelector('input#fnum'); 
+let lista = document.querySelector('select#flista');
+let res = document.querySelector('div#res');
+let valores = [];
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true;
+    } else {
+        return false;
     }
 }
-function inLista(n,l){
-    if (l.indexOf(Number(n)) != -1){
-        return true
-    }else{
-        return false
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true;
+    } else {
+        return false;
     }
 }
-function adicionar(){
-    if(isNumero(num.value) && !inLista(num.value)){
-        window.alert('Tudo OK')
+
+function adicionar() {
+    let num = mum.value; 
+
+    
+    if (isNumero(num) && !inLista(num, valores)) {
+        
+        valores.push(Number(num)); 
+
+        
+        let item = document.createElement('option');
+        item.text = `Valor ${num} adicionado.`;
+
+
+        lista.appendChild(item); 
+        res.innerHTML = ''; 
+
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.');
+    }
+
+    
+    mum.value = '';
+    mum.focus();
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        window.alert('Adicione valores antes de finalizar')
     }else{
-        window.alert('Valor inválido ou já encontrado na lista.')
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma=0
+        let media=0
+        for (let pos in valores){
+            soma+=valores[pos]
+            if (valores[pos] > maior){
+                maior=valores[pos]
+            }
+            if (valores[pos] < menor){
+                menor=valores[pos]
+            }
+        }
+        media = soma/tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao, todo temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
     }
 }
---
+++ aulas/aula17/objeto01.js
@@ -0,0 +1,1 @@
+let amigo = {nome:'jose',sexo:'M',peso: 85.4}
